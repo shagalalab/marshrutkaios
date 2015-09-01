77efcb4e6061aa4da6ab79cd2008671fa84fa61d
Aziz Murtazaev <muraziz@gmail.com>
2015-09-01 23:11:22 +0500

    First commit

---
+++ Marshrutka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marshrutka
+//
+//  Created by Aziz Murtazaev on 9/1/15.
+//  Copyright (c) 2015 Shagala Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
